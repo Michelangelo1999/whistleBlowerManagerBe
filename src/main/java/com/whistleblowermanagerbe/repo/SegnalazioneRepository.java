9483a306b8826423211a7558a7c704f02cb688a9
Michelangelo1999 <mikidenicola007@gmail.com>
2023-11-02 22:53:28 +0100

    assegna segnalazioni

--- src/main/java/com/whistleblowermanagerbe/repo/SegnalazioneRepository.java
+++ src/main/java/com/whistleblowermanagerbe/repo/SegnalazioneRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Repository
 public interface SegnalazioneRepository extends JpaRepository<Segnalazione, Integer> {
 
-    @Query(value = "select * from segnalazione where key_16 = :key16")
+    @Query(value = "select * from segnalazione where key_16 = :key16", nativeQuery = true)
     Optional<Segnalazione> findByKey16(String key16);
     @Query(value = "select count(id) from segnalazione", nativeQuery = true)
     Integer countSegnalazioni();
